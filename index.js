f0c8e45fc48f1d904caae30715cf1e43cb9760f4
ah-rifat <rifat266@gmail.com>
2023-05-02 16:22:05 +0600

    install cors and setup has done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const chefInfo = require('./data/chefsData.json')
+
+app.use(cors)
 
 app.get('/chefs', (req, res) => {
     res.send(chefInfo)
